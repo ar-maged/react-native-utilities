71951d8d6f93dc60c4f2f7f4fa44206580a829f3
A.R. Maged <abdulrahman.maged95@gmail.com>
2017-05-17 02:34:38 +0200

    :wrench: Export getCurrentCoordinates() via Location

---
+++ Location.js
@@ -0,0 +1,25 @@
+import 'react-native';
+
+/**
+  * Get current coordinates
+  */
+const getCurrentCoordinates = ({ timeout, maximumAge }) => {
+  const options = {
+    timeout: timeout || 20000,
+    maximumAge: maximumAge || 1000
+  };
+
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      position =>
+        resolve({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        }),
+      error => reject(error),
+      options
+    );
+  });
+};
+
+export default { getCurrentCoordinates };
